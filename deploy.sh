3b56369d994179f68a6b66a514a03509cb3811a7
unknown <padelis.theodosiou@gmail.com>
2020-07-27 00:11:11 +0300

    Deploy application

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git add -A
+git commit -m 'Deploy application'
+git push -f git@github.com:ThPadelis/covid-19.git master:gh-pages
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/covid-19/" : "/",
   css: {
     loaderOptions: {
       scss: {
