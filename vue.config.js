05102b7bbcac73ea9a45b333fbc4637c1a6f0f86
ThPadelis <padelis.theodosiou@gmail.com>
2021-01-16 20:24:21 +0200

    Minor updates
    - Fixed minor issues
    - Deployed the application under the /apps/ folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/covid-19/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/apps/covid/" : "/",
   css: {
     loaderOptions: {
       scss: {
